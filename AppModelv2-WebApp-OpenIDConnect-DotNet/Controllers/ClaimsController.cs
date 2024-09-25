be8bab7983e9243ef8074517fb85337954ba9f02
Administrador <Administrador@NL-SERVER>
2024-09-25 03:13:26 +0100

    Agregar archivos de proyecto.

---
+++ AppModelv2-WebApp-OpenIDConnect-DotNet/Controllers/ClaimsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Graph;
+using Microsoft.Identity.Web;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppModelv2_WebApp_OpenIDConnect_DotNet.Controllers
+{
+    [Authorize]
+    public class ClaimsController : Controller
+    {
+        /// <summary>
+        /// Add user's claims to viewbag
+        /// </summary>
+        /// <returns></returns>
+        public async Task<ActionResult> Index()
+        {
+            var userClaims = User.Identity as System.Security.Claims.ClaimsIdentity;
+
+            // You get the user’s first and last name below:
+            ViewBag.Name = userClaims?.FindFirst("name")?.Value;
+
+            // The subject/ NameIdentifier claim can be used to uniquely identify the user across the web
+            ViewBag.Subject = userClaims?.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+
+            // TenantId is the unique Tenant Id - which represents an organization in Azure AD
+            ViewBag.TenantId = userClaims?.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid")?.Value;
+
+            // You can also call Microsoft Graph (with incremental consent)
+            try
+            { 
+                var me = await this.GetGraphServiceClient().Me.GetAsync();
+                ViewBag.Username = me.DisplayName;
+            }
+            catch (ServiceException graphEx) when (graphEx.InnerException is MicrosoftIdentityWebChallengeUserException)
+            {
+                HttpContext.GetOwinContext().Authentication.Challenge(OpenIdConnectAuthenticationDefaults.AuthenticationType);
+                return View();
+            }
+
+            return View();
+        }
+    }
+}
---
+++ AppModelv2-WebApp-OpenIDConnect-DotNet/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Identity.Web.OWIN;
+using Microsoft.Identity.Web;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Client;
+using Microsoft.Identity.Web.TokenCacheProviders.InMemory;
+
+[assembly: OwinStartup(typeof(AppModelv2_WebApp_OpenIDConnect_DotNet.Startup))]
+
+namespace AppModelv2_WebApp_OpenIDConnect_DotNet
+{
+    public class Startup
+    {
+        /// <summary>
+        /// Configure OWIN to use OpenIdConnect 
+        /// </summary>
+        /// <param name="app"></param>
+        public void Configuration(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            OwinTokenAcquirerFactory factory = TokenAcquirerFactory.GetDefaultInstance<OwinTokenAcquirerFactory>();
+
+            app.AddMicrosoftIdentityWebApp(factory);
+            factory.Services
+                .Configure<ConfidentialClientApplicationOptions>(options => { options.RedirectUri = "https://localhost:44368/"; })
+                .AddMicrosoftGraph()
+                .AddInMemoryTokenCaches();
+            factory.Build();
+        }
+    }
+}
